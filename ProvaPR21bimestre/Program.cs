a0f0f8de3c1b3dae9ea2a6fac0161b09ffd6f849
CJ301679X <CJ301679X@LAB01-PC33>
2023-04-19 12:53:55 -0300

    Adicionar arquivos de projeto.

---
+++ ProvaPR21bimestre/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaPR21bimestre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////1
+            //Console.WriteLine("Digite numero inteiri.")
+
+            //    int n1;
+            //n1 = int.Parse(Console.ReadLine());
+
+            //if (n1 < 0) {
+
+            //    n1 = n1
+            //        }
+            //Console.WriteLine("O valor absoluto é;");
+            // Console. Read.line();
+
+
+
+            //2 
+            //int num;
+            //Console.WriteLine("Digite um numero inteiro: ");
+            //num = int.Parse(Console.ReadLine());
+
+            //if (num % 3 == 0 && num % 5 == 0) {
+            //    Console.WriteLine(num + " é multiplo de 3 e 5.");
+            //else if (num % 4 == 0 && num % 7 == 0) {
+            //        Console.WriteLine(num + " multiplo de 4 e 7. ");
+            //    } else {
+            //        if (num % 3 == 0)
+            //            Console.WriteLine(num + "é divisivel por 3.");
+            //    }
+            //    if (num % 4 == 0) {
+            //        Console.WriteLine(num + "é divisivel por 4");
+            //    }
+            //    if (num % 5 == 0) {
+            //        Console.WriteLine(num + "é divisivel por 5.");
+            //    }
+            //    if (num % 7 == 0) {
+            //        Console.WriteLine(num + "é divisivel por 7.");
+            //    }
+            //    if (num % 3 != 0 && num % 4 != 0 && num % 7 != 0) {
+            //        Console.WriteLine("Não é possivel dividir " + "por 3, 4, 5 ou 7.");
+
+            //    }
+            //    Console.ReadLine();
+
+
+            ////3
+            //int n1, n2, n3, maior;
+
+            //Console.WriteLine("digite um numero:");
+            //n1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("didite o segundo numero:")
+            //n2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("digite o ultimo numero:"); 
+            //n3 = int.Parse(Console.ReadLine());
+
+            //if (n1 > n2) {
+            //    maior = n1;
+            //} else {
+            //    maior = n2;
+            //}
+            //if (n3 > maior) {
+            //    maior = n3;
+            //}
+            //Console.WriteLine(" o maior numero é: " + maior);
+
+            //Console.ReadLine();
+
+            ////4 
+            //Console.Write("Digite um número de 1 a 7: ");
+            //int diaSemana = int.Parse(Console.ReadLine());
+
+            //switch (diaSemana)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Você assiste aula de Matemática aos domingos.");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Você assiste aula de Português às segundas.");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Você assiste aula de História às terças.");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Você assiste aula de Geografia às quartas.");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Você assiste aula de Física às quintas.");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Você assiste aula de Química às sextas.");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Você assiste aula de Biologia aos sábados.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Número inválido, digite um número de 1 a 7.");
+            //        break;
+            //}
+
+            //// Exibe mensagem adicional para finais de semana
+            //if (diaSemana == 1 || diaSemana == 7)
+            //{
+            //    Console.WriteLine("Aos finais de semana, gosto de ir ao cinema e passear com amigos.");
+            //}
+
+
+            ////5
+
+            //int n, maior,
+
+           
+
+
+
+
+
+
+
+
+
+
+
+                    
+          
+                
+
+            
+
+
+            
+
+
+            
+
+   
+            
+            
+
+        }
+    }
+}
